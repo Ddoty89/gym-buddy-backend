fd53b48cea074e221a4b3d16d98c90e5645de253
Doty's <dotys@Dotyss-iMac.local>
2018-02-16 15:22:40 -0600

    adding test

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/jwt-auth-demo';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3000;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'dominicandeliana';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
