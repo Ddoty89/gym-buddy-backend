ab81faa80f42b07cc45aebdcadcf484dc0609a75
Doty's <dotys@Dotyss-iMac.local>
2018-02-21 19:47:06 -0600

    update

---
+++ config.js
@@ -0,0 +1,3 @@
+const CLIENT_ORIGIN = 'localhost:3000';
+
+module.exports = {CLIENT_ORIGIN};
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+  cors({
+		origin: CLIENT_ORIGIN
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +18,3 @@ res.json({ok: true});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
-
-
-
