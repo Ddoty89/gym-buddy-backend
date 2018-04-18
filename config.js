7ed5dbf99ed8c99d71ec3bb47b0813cf213e9ab5
Derek Doty <ddoty.61414@gmail.com>
2018-04-18 12:50:32 -0500

    update to local settings in config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL || 
-    '';
+    'mongo ds153978.mlab.com:53978/gym-buddy-capstone -u ddoty89 -p dotysan815';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'domandellie';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5d';
