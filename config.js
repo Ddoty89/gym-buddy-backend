59fd57168374a3acc16c985a878b2db5df0cdfb5
Derek Doty <ddoty.61414@gmail.com>
2018-04-21 14:00:23 -0400

    update to config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL || 
-    'mongo ds153978.mlab.com:53978/gym-buddy-capstone -u ddoty89 -p dotysan815';
+    'mongodb://ddoty89:dotysan815@ds153978.mlab.com:53978/gym-buddy-capstone';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'domandellie';
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5d';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
